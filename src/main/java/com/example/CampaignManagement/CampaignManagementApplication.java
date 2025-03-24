c687ce004f84422b139b3703899f3c1158581fca
Wojciech Schabowki <swojciech35@gmail.com>
2025-03-24 11:48:30 +0100

    init project

---
+++ src/main/java/com/example/CampaignManagement/CampaignManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.CampaignManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignManagementApplication.class, args);
+	}
+
+}
